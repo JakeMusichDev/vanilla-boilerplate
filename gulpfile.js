9b52d6c36517be94176286dbe0970fc0f136893f
Jake Musich <jake.musich@infomentum.co.uk>
2019-08-08 17:37:21 +0100

    swiper 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,81 @@
-var {src, dest, series, watch} = require('gulp'),
-    uglify = require('gulp-uglify'),
-    terser = require('gulp-terser'),
-    babel = require('gulp-babel'),
-    plumber = require('gulp-plumber'),
-    sass = require('gulp-sass'),
-    cssbeautify = require('gulp-cssbeautify',),
-    del = require('del');
+var {
+  src,
+  dest,
+  series,
+  watch
+} = require('gulp'),
+  uglify = require('gulp-uglify'),
+  terser = require('gulp-terser'),
+  babel = require('gulp-babel'),
+  plumber = require('gulp-plumber'),
+  sass = require('gulp-sass'),
+  cssbeautify = require('gulp-cssbeautify', ),
+  imagemin = require("gulp-imagemin"),
+  del = require('del');
 
 var settings = {
-    clean:true,
-    styles:true
+  clean: true,
+  styles: true
 };
 
 var paths = {
-    input: 'src/',
-    output: 'dist/',
-    scripts: {
-      input: 'src/**/*.js',
-      output: './dist/js/'
-    },
-    styles: {
-      input: 'src/sass/**/*.scss',
-      output: './dist/css/'
-    }
+  input: 'src/',
+  output: 'dist/',
+  scripts: {
+    input: 'src/**/*.js',
+    output: './dist/js/'
+  },
+  styles: {
+    input: 'src/sass/**/*.scss',
+    output: './dist/css/'
+  },
+  assets: {
+    input: 'src/assets/*.+(png|jpg|jpeg|gif|svg)',
+    output: './dist/assets'
+  }
 };
 
-var cleanDist = function(done) {
-    if (!settings.clean) return done();
+var cleanDist = function (done) {
+  if (!settings.clean) return done();
 
-    del.sync([paths.output]);
-    
-    return done();
+  del.sync([paths.output]);
+
+  return done();
 };
 
-var javascript = function() {
-    return src(paths.scripts.input)
-        .pipe(plumber())
-        .pipe(babel({
-          presets: [['@babel/env', { modules: false }]]
-        }))
-        .pipe(terser())
-        .pipe(dest(paths.scripts.output))
+var javascript = function () {
+  return src(paths.scripts.input)
+    .pipe(plumber())
+    .pipe(babel({
+      presets: [
+        ['@babel/env', {
+          modules: false
+        }]
+      ]
+    }))
+    .pipe(terser())
+    .pipe(dest(paths.scripts.output))
 };
 
-var styles = function() {
-    return src(paths.styles.input)
-        .pipe(sass().on("error", sass.logError))
-        .pipe(cssbeautify())
-        .pipe(dest(paths.styles.output))
+var styles = function () {
+  return src(paths.styles.input)
+    .pipe(sass().on("error", sass.logError))
+    .pipe(cssbeautify())
+    .pipe(dest(paths.styles.output))
 };
 
-var watchAll = function() {
-    watch(paths.scripts.input, javascript)
-    watch(paths.styles.input, styles);
+var assets = function () {
+  return src(paths.assets.input)
+    .pipe(imagemin())
+    .pipe(dest(paths.assets.output))
 };
 
-exports.default = series(cleanDist, javascript, styles, watchAll);
+var watchAll = function () {
+  watch(paths.scripts.input, javascript)
+  watch(paths.styles.input, styles)
+  watch(paths.assets.input, assets);
+};
+
+exports.default = series(cleanDist, javascript, styles, assets, watchAll);
 exports.watch = watchAll;
 exports.clean = cleanDist;
